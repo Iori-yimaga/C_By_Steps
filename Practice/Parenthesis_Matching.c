e7ba814bbe1ca28f195b6f06ccf39549d8506fae
iori-yimaga <964302865@qq.com>
2020-06-25 15:10:46 +0800

    括号匹配问题

--- Practice/Parenthesis_Matching.c
+++ Practice/Parenthesis_Matching.c
@@ -1,28 +1,34 @@
 #include<stdio.h>
 
+//自定义栈类型的结构体
 typedef struct SqStack {
     char str[100];
     int top;
-}SqStack;
+} SqStack;
 
-void Push(SqStack* S, char c) {
+//出栈操作
+void Push(SqStack *S, char c) {
     S->top++;
     S->str[S->top] = c;
 }
 
-void Pop(SqStack* S, char* c) {
+//入栈
+void Pop(SqStack *S, char *c) {
     *c = S->str[S->top];
     S->top--;
 }
 
-int Empty(SqStack* S) {
+//置空操作
+int Empty(SqStack *S) {
     return S->top == -1;
 }
 
-void Init(SqStack* S) {
+//初始化栈
+void Init(SqStack *S) {
     S->top = -1;
 }
 
+//左匹配括号操作
 char LeftBracket(char right) {
     if (right == ')')
         return '(';
@@ -33,7 +39,8 @@ char LeftBracket(char right) {
     return 0;
 }
 
-int BracketsCheck(char* str) {
+//括号匹配操作
+int BracketsCheck(char *str) {
     SqStack S;
     Init(&S);
     int i = 0;
@@ -42,21 +49,20 @@ int BracketsCheck(char* str) {
             case '(':
             case '[':
             case '{':
-                Push(&S, str[i]);
+                Push(&S, str[i]);                         //如果是左括号，直接入栈
                 i++;
                 break;
             case ')':
             case ']':
             case '}':
-                if (!Empty(&S)) {
-                    char c;
+                if (!Empty(&S)) {                         //如果是右括号，先检查栈是否为空
+                    char c;                               //若不为空，则把顶元素弹出
                     Pop(&S, &c);
                     if (c == LeftBracket(str[i]))
                         i++;
-                    else
+                    else                                  //否则匹配失败
                         return 0;
-                }
-                else
+                } else
                     return 0;
                 break;
             default:
@@ -67,12 +73,11 @@ int BracketsCheck(char* str) {
 }
 
 int main() {
-    char* str = "{[1+2*(5-2)]*4*{2+6}}";
+    char *str = "{[1+2*(5-2)]*4*{2+6}}";
     int pass = BracketsCheck(str);
     if (pass) {
         printf("匹配成功!");
-    }
-    else {
+    } else {
         printf("匹配失败!");
     }
     return 0;
